b3b225721da5b4ab72b5b91c8c6f96d597161a4b
AbrarNitk <abrar.nitk@gmail.com>
2022-01-01 18:12:14 +0530

    user login

---
+++ dj/authentication/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('users/login', views.login_api)
+]
--- dj/authentication/views.py
+++ dj/authentication/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, logout, login
+from . import models
 
 # Create your views here.
+
+
+def login_api(req):
+    body = json.loads(req.body)
+    u = authenticate(email=body['email'], password=body['password'])
+    if u is None:
+        return JsonResponse({'data': {}, 'success': False}, status=401)
+    return JsonResponse({'data': {'token': u.token}, 'success': True}, status=200)
+
+
+def register(req):
+    body = json.loads(req.body)
+    u = models.User.objects.create_user(email=body['email'], password=body['password'])
